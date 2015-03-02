75031902622879c26733fae8f10a4fec9b28c088
Jonathan Carroll <jonathaniscarroll@gmail.com>
2015-03-02 18:05:59 +0100

    lots of changes for everything

---
+++ HQ/Assets/MoveLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLight : MonoBehaviour {
+
+	public Transform x;
+
+	
+
+	// Update is called once per frame
+	void Update () {
+		if (x.position.z < 5) {
+
+						transform.Translate (Vector3.forward * Time.deltaTime * 3);
+
+				} else if (x.position.z > 5) {
+						
+				transform.position = new Vector3(0,1,-6);		
+		}
+	
+	}
+}
